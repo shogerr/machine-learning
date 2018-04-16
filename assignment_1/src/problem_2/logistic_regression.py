7cfaa50700dee70d045bfe278826100540f72e30
shogerr <shogerr@oregonstate.edu>
2018-04-15 17:20:00 -0700

    growing epsilon

--- assignment_1/src/problem_2/logistic_regression.py
+++ assignment_1/src/problem_2/logistic_regression.py
@@ -8,113 +8,140 @@ test_data_file = "usps-4-9-test.csv"
 
 # exit condition
 epsilon = 25
+
 # learning rate
 eta = .02
 
 def parse_data(filename):
-	data = []
+    data = []
 
-	with open(filename, 'r') as f:
-		reader = csv.reader(f)
-		for line in reader:
-			data.append(line)
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            data.append(line)
 
-	data = [[float(x) for x in t] for t in data]
-	return data
+    data = [[float(x) for x in t] for t in data]
+    return data
 
 
 def create_matrices(filename):
-	data = parse_data(filename)
-	
-	#pop trailing 0s and 1s representing 4s and 9s
-	Y = []
-	[Y.append(x.pop()) for x in data]
+    data = parse_data(filename)
+    
+    #pop trailing 0s and 1s representing 4s and 9s
+    Y = []
+    [Y.append(x.pop()) for x in data]
 
-	# make matrix of features 
-	X = np.matrix([[x/255 for x in t] for t in data])
+    # make matrix of features 
+    X = np.matrix([[x/255 for x in t] for t in data])
 
-	# make matrix of results and Tranpose into a column
-	Y = np.matrix(Y).T
+    # make matrix of results and Tranpose into a column
+    Y = np.matrix(Y).T
 
-	return X, Y
+    return X, Y
 
 
 # calculates sigmoid expression
 # weight: one dimensional matrix row/column
 # X: one dimensional matrix orientation opposite weight
 def sigmoid(weight, X):
-	return 1/(1 + np.exp(-np.dot(weight, X)))
+    return 1/(1 + np.exp(-np.dot(weight, X)))
 
 
 # runs training algorithm from given data filename
-def train(data_filename):
-	X, Y = create_matrices(data_filename)
+def train(data_filename, regularize=False, find_accuracy=False, l=0):
+    print(l)
+    X, Y = create_matrices(data_filename)
 
-	w = np.zeros(X.shape[1])
+    w = np.zeros(X.shape[1])
 
-	iteration = 0
-	accuracy_file = open("accuracy.csv", "w+")
-	# pseudo do while loop
-	while True:
-		# reset gradient
-		gradient = np.zeros(X.shape[1])
-		for i in range(X.shape[0]):
-			# A1 flattens X[i] to match shape of w.T
-			y_hat = sigmoid(w.T, X[i].A1)
+    iteration = 0
+    accuracy_file = open("accuracy.csv", "w+")
+    # pseudo do while loop
+    while True:
+        # reset gradient
+        gradient = np.zeros(X.shape[1])
+        for i in range(X.shape[0]):
+            # A1 flattens X[i] to match shape of w.T
+            y_hat = sigmoid(w.T, X[i].A1)
 
-			# only looking for 1 or 0
-			if y_hat >= .5:
-				y_hat = 1
+            # perform regularization
+            if regularize:
+                y_hat += .5 * l * np.linalg.norm(w, 2)
 
-			#calculate change in gradient
-			gradient = gradient + (y_hat - Y[i])*X[i]
-		
-		# modify weights with calculated gradient
-		w = w - (eta*gradient)
-		# above calculation adds dimenision to w, needs to be flattened again
-		w = w.A1
+            # only looking for 1 or 0
+            if y_hat >= .5:
+                y_hat = 1
 
-		iter_rate = test(test_data_file, w)
-		accuracy_file.write(str(iteration) + "," + str(iter_rate) + "\n")
-		iteration += 1
+            #calculate change in gradient
+            gradient = gradient + (y_hat - Y[i])*X[i]
 
-		# do while conditional
-		if np.linalg.norm(gradient) <= epsilon:
-			break
+        # modify weights with calculated gradient
+        w = w - (eta*gradient)
+        # above calculation adds dimenision to w, needs to be flattened again
+        w = w.A1
 
-	accuracy_file.close()
+        if find_accuracy:
+            train_iter_rate = test(train_data_file, w)
+            iter_rate = test(test_data_file, w)
+            accuracy_file.write(str(iteration) + ',' + str(train_iter_rate) + ',' + str(iter_rate) + '\n')
+            iteration += 1
 
-	return w
+        # do while conditional
+        print(np.linalg.norm(gradient))
+        if np.linalg.norm(gradient) <= epsilon:
+            break
+
+    accuracy_file.close()
+
+    return w
 
 
 # performs test using weights derived from train
 # w: single dimenisional matrix should same orientation as X
 def test(data_filename, w):
-	X, Y = create_matrices(data_filename)
+    X, Y = create_matrices(data_filename)
 
-	total = 0
-	success = 0
-	for i in range(X.shape[0]):
-		# run sigmoid function uses weights
-		y_hat = sigmoid(w.T, X[i].A1)
-		# check if it guessed correctly
-		if y_hat >= .5 and Y[i] == 1:
-			success += 1
-		elif y_hat < .5 and Y[i] == 0:
-			success += 1
-		total += 1
+    total = 0
+    success = 0
+    for i in range(X.shape[0]):
+        # run sigmoid function uses weights
+        y_hat = sigmoid(w.T, X[i].A1)
+        # check if it guessed correctly
+        if y_hat >= .5 and Y[i] == 1:
+            success += 1
+        elif y_hat < .5 and Y[i] == 0:
+            success += 1
+        total += 1
 
-	# calculate success rate
-	rate = float(success)/float(total)
-	return rate
-		
+    # calculate success rate
+    rate = float(success)/float(total)
+    return rate
+        
 
 if __name__ == "__main__":
-	# run training algorithm
-	start_time = time.time()
-	w = train(train_data_file)
-	print("Run time: " + str(time.time() - start_time))
+    # run training algorithm
+    start_time = time.time()
+    w = train(train_data_file, find_accuracy=True)
+    print("Run time: " + str(time.time() - start_time))
 
-	# perform tests on learned weights
-	rate = test(test_data_file, w)
-	print("success rate: " + str(rate*100))
+    # perform tests on learned weights
+    rate = test(test_data_file, w)
+    print("success rate: " + str(rate*100))
+
+    # perform tests for different values of lambda
+    l_test = [-10, 1, 10, 100, 1000]
+    test_results = []
+    train_result = []
+    for l in l_test:
+        print(l)
+        w = train(train_data_file, regularize=True, l=l)
+        test_results.append(test(w, train_data_file))
+        train_results.append(test(w, test_data_file))
+
+    with open('lamda_results.csv', 'w') as f:
+        for i in range(len(l_test)):
+            f.write(str(l_test[i]) + ',' + str(train_results[i]) + ',' + str(test_results[i]) + '\n')
+
+
+
+
