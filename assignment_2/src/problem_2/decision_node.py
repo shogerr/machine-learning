1d22d628a9ab7f5a8c8a10f720e57edac279ad6a
Alex Ruef <ruefa@oregonstate.edu>
2018-04-28 14:27:37 -0700

    changed DecisionNode children to nodes

--- assignment_2/src/problem_2/decision_node.py
+++ assignment_2/src/problem_2/decision_node.py
@@ -2,5 +2,13 @@ class DecisionNode:
 	gain = 0
 	X = []
 	y = []
+	decision_index = None
+	depth = 0
+	# left and right will eventually be DecisionNodes themself
 	child_left = ([], [])
 	child_right = ([], [])
+
+	def __init__(self, X, y, level):
+		self.X = X
+		self.y = y
+		self.depth = level
--- assignment_2/src/problem_2/decision_tree.py
+++ assignment_2/src/problem_2/decision_tree.py
@@ -4,6 +4,9 @@ from decision_node import *
 MALIGNANT = 1
 BENIGN = -1
 
+# decision tree depth limits
+d = [1, 2, 3, 4, 5, 6]
+
 # loads data from text file with comma delimiter into numpy matrices
 def loadMatrices(file_name):
 	data = np.loadtxt(open(file_name, "rb"), delimiter=',').astype("float")
@@ -13,19 +16,20 @@ def loadMatrices(file_name):
 
 	return X, y
 
+
 # generates a graph file to generate an image with graphviz
 def genGraphFile(file_name, head_node):
 	values = value_counter(head_node.y, ret_dict=True)
-	main_label = "S | " + str(values[MALIGNANT]) + " : " + str(values[BENIGN])
+	main_label = "ROOT | " + str(values[MALIGNANT]) + " : " + str(values[BENIGN])
 
-	values = value_counter(head_node.child_left[1], ret_dict=True)
+	values = value_counter(head_node.child_left.y, ret_dict=True)
 	if values[MALIGNANT] > values[BENIGN]:
 		left_label = "MALIGNANT | "
 	else:
 		left_label = "BENIGN | "
 	left_label += str(values[MALIGNANT]) + " : " + str(values[BENIGN])
 
-	values = value_counter(head_node.child_right[1], ret_dict=True)
+	values = value_counter(head_node.child_right.y, ret_dict=True)
 	if values[MALIGNANT] > values[BENIGN]:
 		right_label = "MALIGNANT | "
 	else:
@@ -94,16 +98,22 @@ def info_gain(y, y1, y2):
 	return entropy(y) - p1*entropy(y1) - p2*entropy(y2)
 
 
-def binary_split(X, y):
+def binary_split(node):
 	# used to track the best information gain
-	node = DecisionNode()
-	node.y = y
+	#node = DecisionNode()
+	#node.y = y
 
+	X = node.X
+	y = node.y
+
+	#iterate through columns in X
 	for i in range(X.shape[1]):
 		y_left, y_right = [], []
 		X_left, X_right = [], []
 
+		# iterate through rows in X
 		for j in range(X.shape[0]):
+			# split data based on X value
 			if X[j][i] >= .5:
 				y_right.append(y[j])
 				X_right.append(X[j])
@@ -111,11 +121,14 @@ def binary_split(X, y):
 				y_left.append(y[j])
 				X_left.append(X[j])
 
+		# calculate gain with particular binary split
 		gain = info_gain(y, y_left, y_right)
+		# save binary split if it is better than other iterations
 		if gain > node.gain:
 			node.gain = gain
-			node.child_left = (X_left, y_left)
-			node.child_right = (X_right, y_right)
+			node.index = i
+			node.child_left = DecisionNode(X_left, y_left, node.depth)
+			node.child_right = DecisionNode(X_right, y_right, node.depth)
 
 	return node
 
@@ -128,11 +141,14 @@ def train(file_name):
 	# normalize data in X
 	X = normalize(X)
 
-	# create node based on best binary_split
-	node = binary_split(X, y)
-	print(node.gain)
+	# create head/root node
+	head_node = DecisionNode(X, y, 0)
 
-	genGraphFile("stump.gv", node)
+	# create node based on best binary_split
+	head_node = binary_split(head_node)
+	print(head_node.gain)
+
+	genGraphFile("stump.gv", head_node)
 
 
 if __name__ == "__main__":
