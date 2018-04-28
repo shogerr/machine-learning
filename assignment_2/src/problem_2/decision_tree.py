f09e2bda6c16240e7371c6c3fa1ca2dd964a59e0
Alex Ruef <ruefa@oregonstate.edu>
2018-04-27 18:09:11 -0700

    added methods for entropy calculation

--- assignment_2/src/problem_2/decision_tree.py
+++ assignment_2/src/problem_2/decision_tree.py
@@ -9,9 +9,47 @@ def loadMatrices(file_name):
 
 	return X, y
 
+
 # normalizes x matrix values to be between 0 and 1
 def normalize(x):
 	return x / x.max(axis=0)
+
+
+# counts occurences of values
+# returns list of each value / total
+def value_counter(y):
+	values = {}
+	total = 0
+	# count occurences of each value found in y
+	for line in y:
+		if line in values:
+			values[line] += 1
+		else:
+			values[line] = 1
+		total += 1
+
+	# write values found / total values into a list
+	results = []
+	for key in values:
+		if values[key] > 0:
+			results.append(values[key]/float(total))
+
+	return results
+
+
+# performs uncertainty calculation
+# y: numpy single column array
+def entropy(y):
+	# get p1 and p2 probability
+	values = value_counter(y)
+
+	# perform entropy calculation
+	E = 0
+	for p in values:
+		E -= p * np.log2(p)
+
+	return E
+
 
 if __name__ == "__main__":
 	# load test data
@@ -19,6 +57,5 @@ if __name__ == "__main__":
 
 	# normalize data in X
 	X = normalize(X)
-	print(X)
-	print()
-	print(y)
+
+	entropy(y)
