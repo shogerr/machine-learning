813229452f98142012e9c5f2a2bf19e27ce17afc
Alex Ruef <ruefa@oregonstate.edu>
2018-04-28 14:37:47 -0700

    added decision and method to calculate it

--- assignment_2/src/problem_2/decision_node.py
+++ assignment_2/src/problem_2/decision_node.py
@@ -1,3 +1,6 @@
+MALIGNANT = 1
+BENIGN = -1
+
 class DecisionNode:
 	# information gain
 	gain = 0
@@ -9,8 +12,22 @@ class DecisionNode:
 	# left and right child nodes
 	child_left = None
 	child_right = None
+	decision = None
 
 	def __init__(self, X, y, level):
 		self.X = X
 		self.y = y
 		self.depth = level
+		self.calcMajority()
+
+	def calcMajority(self):
+		counter = {}
+		counter[MALIGNANT] = 0
+		counter[BENIGN] = 0
+		for line in self.y:
+			counter[line] += 1
+
+		if counter[MALIGNANT] > counter[BENIGN]:
+			decision = MALIGNANT
+		else:
+			decision = BENIGN
