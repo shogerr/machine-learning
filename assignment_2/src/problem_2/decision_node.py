4b5319b82d5825da571e6f9b01f30007d14890c1
Alex Ruef <ruefa@oregonstate.edu>
2018-04-28 14:28:59 -0700

    added comments

--- assignment_2/src/problem_2/decision_node.py
+++ assignment_2/src/problem_2/decision_node.py
@@ -1,12 +1,14 @@
 class DecisionNode:
+	# information gain
 	gain = 0
 	X = []
 	y = []
+	# index of X column that decided the split
 	decision_index = None
 	depth = 0
-	# left and right will eventually be DecisionNodes themself
-	child_left = ([], [])
-	child_right = ([], [])
+	# left and right child nodes
+	child_left = None
+	child_right = None
 
 	def __init__(self, X, y, level):
 		self.X = X
