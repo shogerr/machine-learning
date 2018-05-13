930fed90ef377acd98d2fa01352e0dfb7b5454f2
Alex Ruef <ruefa@oregonstate.edu>
2018-05-13 12:26:37 -0700

    started mlp. added imports

---
+++ assignment_3/src/mlp.py
@@ -0,0 +1,15 @@
+#%matplotlib inline
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+import numpy as np
+#import matplotlib.pyplot as plt
+import seaborn as sns
+
+cuda = torch.cuda.is_available()
+print('Using PyTorch version:', torch.__version__, 'CUDA:', cuda)
