6a98369a8706aca4714d129956c79a237e03edef
Alex Ruef <ruefa@oregonstate.edu>
2018-05-13 13:08:12 -0700

    fixed data loader

--- assignment_3/src/mlp.py
+++ assignment_3/src/mlp.py
@@ -13,3 +13,27 @@ import seaborn as sns
 
 cuda = torch.cuda.is_available()
 print('Using PyTorch version:', torch.__version__, 'CUDA:', cuda)
+
+DATA_DIR = './'
+
+
+def load_data():
+	batch_size = 32
+	# not sure what to do with mean and std. copied from example
+	norm_mean = (0.1307,)
+	norm_std = (0.3081,)
+
+	# cuda is false so will be {}
+	kwargs = {'num_workers' : 1, 'pin_memory' : True} if cuda else {}
+
+	# load training data
+	train_loader = torch.utils.data.DataLoader(
+		datasets.CIFAR10(DATA_DIR, train=True, download=True,
+						 transform=transforms.Compose([
+							transforms.ToTensor(),
+							transforms.Normalize(norm_mean, norm_std)
+						 ])),
+		batch_size=batch_size, shuffle=False, **kwargs)
+
+if __name__ == '__main__':
+	load_data()
